0023281b526821d06d04e05ebaa4e17c2cbf94bc
Dries Langsweirdt <dries@langsweirdt.be>
2023-09-26 22:22:57 +0200

    Hero test

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,14 @@
 import { logSomething } from './module';
 
 logSomething('Hello World');
+
+function smoothScroll(){
+    console.log('in scroll')
+    document.querySelector("#praktijk").scrollIntoView({
+        behavior: 'smooth'
+    });
+}
+
+document.getElementById("menu-item-1").addEventListener("click", smoothScroll());
+
+
