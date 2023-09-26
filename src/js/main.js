2d92ec485dc7c8689844982f9103c94b8fae40b5
Dries Langsweirdt <dries@langsweirdt.be>
2023-09-26 22:25:30 +0200

    Disable js

--- src/js/main.js
+++ src/js/main.js
@@ -2,13 +2,13 @@ import { logSomething } from './module';
 
 logSomething('Hello World');
 
-function smoothScroll(){
-    console.log('in scroll')
-    document.querySelector("#praktijk").scrollIntoView({
-        behavior: 'smooth'
-    });
-}
-
-document.getElementById("menu-item-1").addEventListener("click", smoothScroll());
+// function smoothScroll(){
+//     console.log('in scroll')
+//     document.querySelector("#praktijk").scrollIntoView({
+//         behavior: 'smooth'
+//     });
+// }
+//
+// document.getElementById("menu-item-1").addEventListener("click", smoothScroll());
 
 
